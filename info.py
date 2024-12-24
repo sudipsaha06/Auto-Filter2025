9f7a2d0db1840ae6c897d2bca43ddbabce51b642
sudipsaha06 <154732963+sudipsaha06@users.noreply.github.com>
2024-12-24 17:28:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ API_HASH = environ.get('API_HASH', '0dd6c10897b85d7f10a8dcdeb74f8b8a')
 BOT_TOKEN = environ.get('BOT_TOKEN', '6484301475:AAHVQo0TocKiqtXdA6krdk_fAKpFzPoQtKo')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5165943027').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/SUDIPSAHA06") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001565949579'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Ott_Request_Group')
