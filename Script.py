8d29414c697722f67b2b114b747be71be4c32d39
sudipsaha06 <154732963+sudipsaha06@users.noreply.github.com>
2024-12-25 17:57:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Ott_Request_Group">𝙅𝙞𝙨𝙨𝙝𝙪 𝙊𝙧𝙞𝙜𝙞𝙣𝙖𝙡𝙨</a>」</b>"""
+    FILE_CAPTION = """<b><a href="https://t.me/Ott_Request_Group">{file_name}</a>\n\n「<a href="https://t.me/Sudip_Tricks">Update Channel</a>」 </b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
